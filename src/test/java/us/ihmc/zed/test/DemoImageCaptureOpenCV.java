f7d9f5039be1d561c8259e74edfa663a4ff8d68a
Dexton Anderson <danderson@ihmc.org>
2024-10-22 13:35:35 -0500

    Fix compile issues

--- src/test/java/us/ihmc/zed/test/DemoImageCaptureOpenCV.java
+++ src/test/java/us/ihmc/zed/test/DemoImageCaptureOpenCV.java
@@ -65,7 +65,7 @@ public class DemoImageCaptureOpenCV {
         init_param.open_timeout_sec(5.0f);
         init_param.async_grab_camera_recovery(false);
         init_param.grab_compute_capping_fps(0);
-        init_param.enable_image_validity_check(false);
+        init_param.enable_image_validity_check(0);
 
         // Open the camera
         int state = sl_open_camera(camera_id, init_param, 0, "", "", 0, "", "", "");
