4c18d887c4f3a580cfe99c838dc36f390a3e6179
ds58 <30220598+ds58@users.noreply.github.com>
2024-10-23 13:15:40 -0500

    Check ZED SDK compatibility version before loading (#4)

--- src/main/java/us/ihmc/zed/library/ZEDJavaAPINativeLibrary.java
+++ src/main/java/us/ihmc/zed/library/ZEDJavaAPINativeLibrary.java
@@ -4,7 +4,16 @@ import us.ihmc.tools.nativelibraries.NativeLibraryDescription;
 import us.ihmc.tools.nativelibraries.NativeLibraryLoader;
 import us.ihmc.tools.nativelibraries.NativeLibraryWithDependencies;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ZEDJavaAPINativeLibrary implements NativeLibraryDescription {
+    private static final int ZED_SDK_COMPATIBILITY_MAJOR_VERSION = 4;
+    private static final int ZED_SDK_COMPATIBILITY_MINOR_VERSION = 2;
+
     @Override
     public String getPackage(OperatingSystem os, Architecture arch) {
         String archPackage = "";
@@ -41,10 +50,60 @@ public class ZEDJavaAPINativeLibrary implements NativeLibraryDescription {
     private static boolean loaded = false;
 
     public static boolean load() {
-        if (!loaded) {
-            ZEDJavaAPINativeLibrary lib = new ZEDJavaAPINativeLibrary();
-            loaded = NativeLibraryLoader.loadLibrary(lib);
+        String installedZEDSDKVersion = getZEDSDKCMakePackageVersion();
+
+        int installedZEDSDKMajorVersion = 0;
+        int installedZEDSDKMinorVersion = 0;
+
+        if (installedZEDSDKVersion != null) {
+            String[] installedZEDSDKVersionParts = installedZEDSDKVersion.split("\\.");
+
+            try {
+                installedZEDSDKMajorVersion = Integer.parseInt(installedZEDSDKVersionParts[0]);
+                installedZEDSDKMinorVersion = Integer.parseInt(installedZEDSDKVersionParts[1]);
+            } catch (NumberFormatException e) {
+                // Ignore
+            }
         }
+
+        if (installedZEDSDKMajorVersion > ZED_SDK_COMPATIBILITY_MAJOR_VERSION ||
+                (installedZEDSDKMajorVersion == ZED_SDK_COMPATIBILITY_MAJOR_VERSION && installedZEDSDKMinorVersion >= ZED_SDK_COMPATIBILITY_MINOR_VERSION)) {
+            if (!loaded) {
+                ZEDJavaAPINativeLibrary lib = new ZEDJavaAPINativeLibrary();
+                loaded = NativeLibraryLoader.loadLibrary(lib);
+            }
+        } else {
+            System.err.println("Incompatible ZED SDK installation ("
+                    + installedZEDSDKVersion + ") found. Please install "
+                    + ZED_SDK_COMPATIBILITY_MAJOR_VERSION + "." + ZED_SDK_COMPATIBILITY_MINOR_VERSION + " or higher.");
+        }
+
         return loaded;
+    }
+
+    public static String getZEDSDKCMakePackageVersion() {
+        String cmakePackageFile;
+
+        if (System.getProperty("os.name").contains("Windows")) {
+            cmakePackageFile = "C:/Program Files (x86)/ZED SDK/zed-config-version.cmake";
+        } else {
+            cmakePackageFile = "/usr/local/zed/zed-config-version.cmake";
+        }
+
+        try (BufferedReader br = new BufferedReader(new FileReader(cmakePackageFile))) {
+            String line;
+            Pattern pattern = Pattern.compile("set\\(PACKAGE_VERSION \"(\\d+\\.\\d+\\.\\d+)\"\\)");
+
+            while ((line = br.readLine()) != null) {
+                Matcher matcher = pattern.matcher(line);
+                if (matcher.find()) {
+                    return matcher.group(1).trim();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
