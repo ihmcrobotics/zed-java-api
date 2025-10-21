637324f05c0044f80ed474b1b5da0adce7c48ae1
Tomasz Bialek <tbialek@ihmc.org>
2025-10-21 10:06:35 -0500

    Download source from fork

--- cppbuild.bash
+++ cppbuild.bash
@@ -5,7 +5,7 @@ mkdir cppbuild
 cd cppbuild
 
 if [ ! -f "main.tar.gz" ]; then
-  curl -L -o main.tar.gz https://github.com/stereolabs/zed-c-api/archive/refs/heads/main.tar.gz
+  curl -L -o main.tar.gz https://github.com/TomaszTB/zed-c-api/archive/refs/heads/copy-all-init-parameters.tar.gz
 fi
 
 tar -xvf main.tar.gz
